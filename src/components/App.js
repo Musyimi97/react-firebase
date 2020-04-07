ad33ef0501aaed3f46c752ff8f848908f0e88b91
Musyimi97 <collinsmusyimi.cm@gmail.com>
2020-04-07 12:58:06 +0300

    updatedd months

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,7 @@ class App extends Component{
       Minute:1,
       second:1,
       Zodiac:'Aries, Taurus, Gemini, Cancer, Leo, Virgo, Libra, Scorpio, Sagittarius, Capricorn, Aquarius, Pisces'.split(','),
+      M_month:["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Spt","Oct","Nov","Dec"],
 
     })
   }
